0b1b02839a3eec7afe406677b023fda961e82282
Habteab <keshi2017@gmail.com>
2021-12-14 11:58:13 -0800

    feat(instructios): add instructions to resend

--- src/components/CustomizeMessage.js
+++ src/components/CustomizeMessage.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import logo from '../images/ecardpic.jpg';
+
 
 function CustomizeMessage() {
     return (
-        <div>
-            Customize message
-        </div>
+        <>
+         <div className="instructions">
+         <img src={logo} className="thumb" alt="logo" 
+         width/>
+         <p><strong>How to Reshare:</strong><br/>
+         Write your own custom message below </p>
+         </div>
+         <div>
+             <label for ></label>
+         </div>
+        </>
     )
 }
 
